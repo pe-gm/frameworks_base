76cb6226a1f222ffe78cece6f628a5bd2482e9e3
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: add API for disabling gestural navigation
    [jhonboy121]: adapted to A12L and A13
    
    Change-Id: Ie91e7738c6b59be47b3f2b1e0418352de6348167

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -721,6 +721,7 @@ public final class NotificationPanelViewController extends PanelViewController {
         }
     };
 
+    private boolean mBlockedGesturalNavigation = false;
     private final CameraGestureHelper mCameraGestureHelper;
     private final KeyguardBottomAreaViewModel mKeyguardBottomAreaViewModel;
     private final KeyguardBottomAreaInteractor mKeyguardBottomAreaInteractor;
@@ -4437,6 +4438,10 @@ public final class NotificationPanelViewController extends PanelViewController {
         mContentResolver.unregisterContentObserver(mSettingsChangeObserver);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     /**
      * Updates notification panel-specific flags on {@link SysUiState}.
      */
@@ -4447,7 +4452,8 @@ public final class NotificationPanelViewController extends PanelViewController {
         }
         mSysUiState.setFlag(SYSUI_STATE_NOTIFICATION_PANEL_EXPANDED,
                         isFullyExpanded() && !isInSettings())
-                .setFlag(SYSUI_STATE_QUICK_SETTINGS_EXPANDED, isInSettings())
+                .setFlag(SYSUI_STATE_QUICK_SETTINGS_EXPANDED,
+                    mBlockedGesturalNavigation || isInSettings())
                 .commitUpdate(mDisplayId);
     }
 
