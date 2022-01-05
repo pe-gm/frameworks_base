3b405e90889e0621f9c9f235089d51e7c405f289
Richard Raya <rdxzv.dev@gmail.com>
2022-01-05 01:48:43 +0000

    core: Introduce GamesProps
    * GamesProps will spoof the device needed to unlock the FPS of the following games:
    
    - Free Fire - Spoof Asus ROG Phone 1 will unlock 90 FPS
    - Shadowgun Legends - Spoof Asus ROG Phone 1 will unlock 90 FPS
    - COD Mobile - Spoof Xperia 5 || will unlock 120 FPS (only on multiplayer mode)
    - PUBG Mobile - Spoof OnePlus 8 Pro will unlock 90 FPS
    - Wild Rift - Spoof OnePlus 8 Pro will unlock 120 FPS
    - Cyber Hunter - Spoof OnePlus 8 Pro will unlock 90 FPS
    - Mobile Legends - Spoof Xiaomi 11T will unlock 120 FPS
    
    Change-Id: I97771369c41ad01898baabe9f5a559368c71d471
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -65,6 +65,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.custom.PixelPropsUtils;
+import com.android.internal.util.custom.GamesProps;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1235,6 +1236,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesProps.setProps(packageName);
         return app;
     }
     
@@ -1254,6 +1256,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesProps.setProps(packageName);
         return app;
     }
 
