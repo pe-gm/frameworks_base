54dd2373c7277e1d905aed85baa6cc4d101da97d
Gustavo Mendes <gusttavo.me@outlook.com>
2023-01-07 13:57:27 -0300

    SystemUI: Kill Wi-Fi/cell tiles again Revert: * e5882c17091f3eb115f3ddb9e16c708237c378c6. * bc0f6bdf34bef7d1a061387e272bc5e0aa7b03e9. * 4da8dc66d1e9fb38eef16b27fe00cc1a2d117c97. Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -286,8 +286,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 qsInfo.description,
                 mSubscriptionInfo.getSubscriptionId(),
                 mCurrentState.roaming,
-                sbInfo.showTriangle,
-                mCurrentState.isDefault);
+                sbInfo.showTriangle);
         callback.setMobileDataIndicators(mobileDataIndicators);
     }
 
@@ -297,6 +296,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         CharSequence qsDescription = null;
 
         if (mCurrentState.dataSim) {
+            // only show QS icons if the state is also default
+            if (!mCurrentState.isDefault) {
+                return new QsInfo(qsTypeIcon, qsIcon, qsDescription);
+            }
+
             if (mCurrentState.showQuickSettingsRatIcon() || mConfig.alwaysShowDataRatIcon) {
                 qsTypeIcon = dataTypeIcon;
             }
