383606118f3bc58a8d59f56c96a845c28827c8a9
Gustavo Mendes <gusttavo.me@outlook.com>
2023-06-13 12:54:47 -0300

    PIHooks: Update redfin fp to May release
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -64,7 +64,7 @@ public class PropImitationHooks {
         "DEVICE", "redfin",
         "PRODUCT", "redfin",
         "MODEL", "Pixel 5",
-        "FINGERPRINT", "google/redfin/redfin:13/TQ1A.230105.001/9292298:user/release-keys"
+        "FINGERPRINT", "google/redfin/redfin:13/TQ2A.230505.002/9891397:user/release-keys"
     );
 
     private static final Map<String, Object> sPixelXLProps = Map.of(
